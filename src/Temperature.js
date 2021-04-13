f22ffa92f68e903b9ed6d252a483972568fd0757
bellamadsen73 <75944916+bellamadsen73@users.noreply.github.com>
2021-04-13 18:15:04 +0200

    Set temperature

--- src/Temperature.js
+++ src/Temperature.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Temperature.css";
 
 
 export default function Temperature() {
-
-  
+  const [ready, setReady] = useState(false)
+ const [temperature, setTemperature] = useState(null); 
+ function handleResponse(response) {
+    setTemperature(Math.round(response.data.main.temp));
+    setReady(true);
+ }
+if(ready) {
   return (
     <div className="row">
       <div className="col-2">
-        <h3 id="temperature">10</h3>
+        <h3 id="temperature">{temperature}</h3>
       </div>
       <div className="col-5">
         <div className="clearfix weather-temperature">
@@ -27,5 +33,13 @@ export default function Temperature() {
       </div>
     </div>
   );
+} else {
+  const apiKey="11a6bd137d38c9eff9b66fb017459c47";
+let city ="London"
+const apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+ 
+  axios.get(apiUrl).then(handleResponse);
+  return "Loading"
+  
 }
-
+}
